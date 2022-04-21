a5628634ab497d1bb74edc97c927036cd9c5f5ad
ElijahLo-ot <elijahchristianloot@gmail.com>
2022-04-21 23:16:10 +0800

    1 binaries uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'course', component: CourseComponent },
+  { path: 'joinnow', component: JoinnowComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { AboutComponent } from './about/about.component';
+
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    CourseComponent,
+    JoinnowComponent,
+    AboutComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
